b841c6d051d07e75e43cd9ba361b64473def39d7
Darshan Gada <gadadarshan@gmail.com>
2020-05-19 22:15:46 +0530

    📦 NEW: Home Page UI

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,13 +7,23 @@ const Nav = () => (
       <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
       <meta name='description' content='Description' />
       <meta name='keywords' content='Keywords' />
-      <title>I ❤️ JSON</title>
+      <title>iLoveJSON | Online JSON Tools for JSON Lovers</title>
 
       <link rel='manifest' href='/manifest.json' />
       <link href='/favs/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
       <link href='/favs/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
       <link rel='apple-touch-icon' href='/favs/apple-icon.png'></link>
       <meta name='theme-color' content='#317EFB' />
+
+      <ul>
+        <li><a href="#compress">Compress</a></li>
+        <li><a href="#beautify">Beautify</a></li>
+        <li><a href="#validate">Validate</a></li>
+        <li><a href="#viewer">Viewer</a></li>
+        <li><a href="#editor">Editor</a></li>
+        <li><a href="#merge">Merge</a></li>
+        <li><a href="#repair">Repair</a></li>
+      </ul>
     </Head>
 
     <style jsx>{`
@@ -27,6 +37,8 @@ const Nav = () => (
       ul {
         display: flex;
         justify-content: space-between;
+        border-bottom: 1px solid #eaeaea;
+        padding-bottom: 0.5rem;
       }
       nav > ul {
         padding: 4px 16px;
@@ -38,7 +50,7 @@ const Nav = () => (
       a {
         color: #067df7;
         text-decoration: none;
-        font-size: 13px;
+        font-size: 15px;
       }
     `}</style>
   </nav>
