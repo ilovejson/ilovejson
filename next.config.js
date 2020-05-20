5d5d13a99c6472825edcf359938cc06a616bc5cd
Darshan Gada <gadadarshan@gmail.com>
2020-05-20 21:11:18 +0530

    📦 NEW: Sass Configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === "production";
+const withPWA = require('next-pwa');
+const withSass = require("@zeit/next-sass");
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public'
-    }
+  pwa: {
+    dest: 'public'
+  }
 })
+
+module.exports = withSass({
+  /* config options here */
+})
+
+module.exports = {
+  poweredByHeader: false,
+  generateEtags: false,
+  distDir: 'dist',
+  // useFileSystemPublicRoutes: false,
+  dontAutoRegisterSw: !isProd,
+}
