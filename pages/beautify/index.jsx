2021c8181c1208f3acefa11c6cb78fde1272aed7
Darshan Gada <gadadarshan@gmail.com>
2020-06-07 18:07:11 +0530

    👌 IMPROVE: Convert to React Hooks

--- pages/beautify/index.jsx
+++ pages/beautify/index.jsx
@@ -1,99 +1,85 @@
-import { Component } from 'react'
+import { useState, useRef } from 'react'
 import Layout from '@components/layout';
 import AlertError from '@components/error';
 import SourceEditor from '@components/source';
 
-class Beautify extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      source: '',
-      output: '',
-      showError: false,
-      copied: false
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const Beautify = () => {
+  const [sourceJSON, setSourceJSON] = useState('');
+  const [outputJSON, setOutputJSON] = useState('');
+  const [showError, setShowError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const textArea = useRef('');
 
-  handleChange(event) {
-    this.setState({ source: event.target.value });
-  }
+  const handleChange = (event) => {
+    setSourceJSON(event.target.value);
+  };
 
-  // Sample JSON: {"foo": "bar", "lorem": true, "numb": 1}
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const str = this.state.source;
+    const str = sourceJSON;
     try {
       if (JSON.parse(str) && !!str) {
-        const outputx = JSON.stringify(JSON.parse(str), undefined, 4);
-        this.setState({ output: outputx, showError: false });
+        setOutputJSON(JSON.stringify(JSON.parse(str), undefined, 4));
+        setShowError(false);
       }
     } catch (e) {
-      this.setState({ showError: true });
+      setShowError(true);
       setTimeout(() => {
-        this.setState({
-          showError: false
-        });
+        setShowError(false);
       }, 5000);
       return false;
     }
   }
 
-  copyCodeToClipboard = () => {
-    const el = this.textArea;
-    el.select();
+  const copyCodeToClipboard = (event) => {
+    textArea.current.select();
     document.execCommand("copy");
-    this.setState({ copied: true });
+    setCopied(true);
     setTimeout(() => {
-      this.setState({
-        copied: false
-      });
+      setCopied(false);
     }, 2000);
   }
 
-  render() {
-    return (
-      <Layout
-        title='Beautify JSON'
-        description='Make JSON Easy to Read.'
-        >
+  return (
+    <Layout
+      title='Beautify JSON'
+      description='Make JSON Easy to Read.'
+    >
 
-        <div className="app mt-5 w-full h-full p-8 font-sans">
-          {/* Row */}
-          <div className="row sm:flex">
-            {/* Col Left */}
-            <SourceEditor>
-              <textarea className="resize-none border rounded text-grey-darkest flex-1 p-2 m-1 bg-transparent" name="source" value={this.state.source} onChange={this.handleChange} />
-            </SourceEditor>
+      <div className="app mt-5 w-full h-full p-8 font-sans">
+        {/* Row */}
+        <div className="row sm:flex">
+          {/* Col Left */}
+          <SourceEditor>
+            <textarea className="resize-none border rounded text-grey-darkest flex-1 p-2 m-1 bg-transparent" name="source" value={sourceJSON} onChange={handleChange} />
+          </SourceEditor>
 
-            {/* Col Right */}
-            <div className="col mt-8 sm:ml-8 sm:mt-0 sm:w-1/2">
-              <div className="box border rounded flex flex-col shadow bg-white box-height">
-                <div className="box__title bg-grey-lighter px-3 py-2 border-b">
-                  <h3 className="text-sm text-grey-darker font-medium inline-flex">Output</h3>
-                  <button className="py-1 px-4 shadow no-underline rounded-sm bg-green-400 text-white font-sans font-semibold text-xs border-green-600 btn-primary hover:text-white hover:bg-green-600 focus:outline-none active:shadow-none float-right" onClick={this.copyCodeToClipboard} disabled={!this.state.output}>Copy</button>
-                  <span className={`mr-2 text-xs text-green-400 float-right align-middle ${this.state.copied ? 'visible' : 'invisible'}`}>Copied to Clipboard!</span>
-                </div>
-                <textarea className="resize text-grey-darkest flex-1 p-2 m-1 bg-transparent" name="output" value={this.state.output} ref={(textarea) => this.textArea = textarea}/>
+          {/* Col Right */}
+          <div className="col mt-8 sm:ml-8 sm:mt-0 sm:w-1/2">
+            <div className="box border rounded flex flex-col shadow bg-white box-height">
+              <div className="box__title bg-grey-lighter px-3 py-2 border-b">
+                <h3 className="text-sm text-grey-darker font-medium inline-flex">Output</h3>
+                <button className="py-1 px-4 shadow no-underline rounded-sm bg-green-400 text-white font-sans font-semibold text-xs border-green-600 btn-primary hover:text-white hover:bg-green-600 focus:outline-none active:shadow-none float-right" onClick={copyCodeToClipboard} disabled={!outputJSON}>Copy</button>
+                <span className={`mr-2 text-xs text-green-400 float-right align-middle ${copied ? 'visible' : 'invisible'}`}>Copied to Clipboard!</span>
               </div>
+              <textarea className="resize text-grey-darkest flex-1 p-2 m-1 bg-transparent" name="output" value={outputJSON} ref={textArea} onChange={() => {}} />
             </div>
           </div>
-
-          {/* Row */}
-          <AlertError message="You've entered invalid JSON." showError={this.state.showError} />
-
-          {/* Row */}
-          <div className="row sm:flex mt-5">
-            <button className={`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mx-auto ${(!this.state.source) ? 'disabled:opacity-75' : ''}`} onClick={this.handleSubmit} disabled={!this.state.source}>
-              <span>Beautify</span>
-            </button>
-          </div>
-
         </div>
-      </Layout>
-    )
-  }
+
+        {/* Row */}
+        <AlertError message="You've entered invalid JSON." showError={showError} />
+
+        {/* Row */}
+        <div className="row sm:flex mt-5">
+          <button className={`bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mx-auto ${(!sourceJSON) ? 'disabled:opacity-75' : ''}`} onClick={handleSubmit} disabled={!sourceJSON}>
+            <span>Beautify</span>
+          </button>
+        </div>
+
+      </div>
+    </Layout>
+  )
 }
 
 export default Beautify
