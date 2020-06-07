c94276e78dda34c3da8d833a921612e471476bfd
Darshan Gada <gadadarshan@gmail.com>
2020-06-07 18:07:58 +0530

    👌 IMPROVE: Add Validations

--- pages/api/jsontoxml.jsx
+++ pages/api/jsontoxml.jsx
@@ -2,7 +2,11 @@ import formidable from 'formidable';
 import { initDirs } from '@utils/initdir';
 import { globals } from '@constants/globals';
 import { uploadToFTP } from '@utils/ftp';
+import { ReE, ReS } from '@utils/reusables';
 const convert = require('xml-js');
+
+const isProd = process.env.NODE_ENV === 'production';
+const cdnUrl = process.env.CDN_URL || '';
 
 const fs = require('fs');
 initDirs();
@@ -16,13 +20,16 @@ export const config = {
   },
 }
 
+const xmlOptions = {
+  compact: true, // BUGGY
+  ignoreComment: true,
+  spaces: 4
+}
+
 // Process a POST request
 export default async (req, res) => {
   if (req.method !== 'POST') {
-    return res.status(403).json({
-      success: false,
-      message: 'I ❤️ JSON. But you shouldn\'t be here.'
-    });
+    return ReE(res, 'I ❤️ JSON. But you shouldn\'t be here.');
   }
 
   const form = await new formidable.IncomingForm();
@@ -30,29 +37,34 @@ export default async (req, res) => {
   form.keepExtensions = true;
   await form.parse(req, async (_err, _fields, files) => {
     if (!(files && files.fileInfo)) {
-      return res.status(400).json({
-        success: false,
-        message: 'I ❤️ JSON. But you forgot to bring something to me.'
-      });
+      return ReE(res, 'I ❤️ JSON. But you forgot to bring something to me.');
     }
 
     var jsonRead = await fs.readFileSync(files?.fileInfo?.path, 'utf8');
 
-    var options = {compact: true, ignoreComment: true, spaces: 4};
-    var xmlOp = await convert.json2xml(jsonRead, options);
+    try {
+      if (JSON.parse(jsonRead) && !!jsonRead) {
+        var xmlOp = await convert.json2xml(jsonRead, xmlOptions);
 
-    const modifiedDate = new Date().getTime();
-    const filePath = `${downloadDir}/${modifiedDate}.xml`;
-    await fs.writeFileSync(filePath, xmlOp, 'utf8');
+        const modifiedDate = new Date().getTime();
+        const filePath = `${downloadDir}/${modifiedDate}.xml`;
+        var toPath = '';
+        await fs.writeFileSync(filePath, xmlOp, 'utf8');
 
-    const toPath = await filePath.replace('dist/downloads/', '');
-    await uploadToFTP(filePath, toPath);
+        if (isProd) {
+          toPath = await filePath.replace('dist/downloads/', '');
+          await uploadToFTP(filePath, toPath);
+        } else {
+          toPath = await filePath.replace('dist/', '');
+        }
 
-    return res.status(200).json({
-      success: true,
-      message: 'I ❤️ JSON. XML Conversion Successful.',
-      data: toPath
-    });
+        return ReS(res, {
+          message: 'I ❤️ JSON. JSON to XML Conversion Successful.',
+          data: `${cdnUrl}/${toPath}`
+        });
+      }
+    } catch (e) {
+      return ReE(res, 'I ❤️ JSON. But you have entered invalid JSON.');
+    }
   });
-
 }
