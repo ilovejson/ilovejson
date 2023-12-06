c5c9efeb5e4c5e5197241abe1d1069d055a78c3d
Darshan Gada <gadadarshan@gmail.com>
2023-12-06 12:34:43 +0530

    feat: remove pwa package and upgrade other packages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 const isProd = process.env.NODE_ENV === "production";
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public'
-  }
-});
 
 module.exports = {
   poweredByHeader: false,
